62d6c04477e3f4e5c01b6faffd2165e338011208
Nicolas Frankel <nicolas@frankel.ch>
2021-01-09 12:30:47 +0100

    Replace duplicate fields by delegate pattern

--- src/main/kotlin/StateModel.kt
+++ src/main/kotlin/StateModel.kt
@@ -1,15 +1,15 @@
-import androidx.compose.runtime.State
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import java.io.File
 
 class StateModel {
 
     private val tracker = mutableStateOf(false)
-    private val pathState = mutableStateOf(System.getProperty("user.home"))
-    private val patternState = mutableStateOf("")
-    private val replacementState = mutableStateOf("")
-    private val filesState: State<List<File>> = derivedStateOf {
+
+    var path: String by mutableStateOf(System.getProperty("user.home"))
+    var pattern: String by mutableStateOf("")
+    var replacement: String by mutableStateOf("")
+
+    val files: List<File> by derivedStateOf {
         tracker.value
         File(path).listFiles()
             ?.filter { !it.isHidden && it.isFile }
@@ -17,27 +17,6 @@ class StateModel {
             ?.sortedBy { it.name }
             ?: emptyList()
     }
-
-    var path: String
-        get() = pathState.value
-        set(value) {
-            pathState.value = value
-        }
-
-    var pattern: String
-        get() = patternState.value
-        set(value) {
-            patternState.value = value
-        }
-
-    var replacement: String
-        get() = replacementState.value
-        set(value) {
-            replacementState.value = value
-        }
-
-    val files: List<File>
-        get() = filesState.value
 
     val candidates: List<File>
         get() {
